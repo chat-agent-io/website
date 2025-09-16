073bc012d4ca09f71ca311ccaa9ae9320ca7db88
Juan David <juand.business@gmail.com>
2025-09-16 20:55:47 +0300

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from 'next';
-import { Onest } from 'next/font/google';
+import type {Metadata} from 'next';
+import {Onest} from 'next/font/google';
 import './globals.css';
-import { FaviconSwitcher } from './utils/FaviconSwitcher';
+import {FaviconSwitcher} from './utils/FaviconSwitcher';
 
 const onest = Onest({
-  subsets: ['latin'],
+    subsets: ['latin'],
+    adjustFontFallback: true,
+    fallback: ['sans-serif']
 });
 
 export const metadata: Metadata = {
-  title: 'ChatAgent - Start replying smarter',
+    title: 'ChatAgent - Start replying smarter',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={onest.className}>
-        <FaviconSwitcher />
+    return (
+        <html lang="en">
+        <body className={onest.className}>
+        <FaviconSwitcher/>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
