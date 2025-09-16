0543358391798c9f3a42981f6a51a590be0c131d
Juan David <juand.business@gmail.com>
2025-09-16 21:20:51 +0300

    Fix font and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
+import type {ReactNode} from 'react'
 import type {Metadata} from 'next';
-import './globals.css';
+
 import {FaviconSwitcher} from './utils/FaviconSwitcher';
+
+//Styles
+import './globals.css';
 
 export const metadata: Metadata = {
     title: 'ChatAgent - Start replying smarter',
+    description: "ChatAgent replies to every Instagram, WhatsApp, or website message — so you don't have to."
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+export default function RootLayout({children}: Readonly<{
+    children: ReactNode;
 }>) {
     return (
         <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+            <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet"/>
+        </head>
         <body>
         <FaviconSwitcher/>
         {children}
