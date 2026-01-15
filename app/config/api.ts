93a28be75cf959113df573290343aec19e9fa78c
Juan David <juand.business@gmail.com>
2026-01-15 14:27:14 +0300

    chore: Github Actions

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,16 +1,6 @@
-const defaultNocoUrl = 'https://app.nocodb.com/api/v2';
 const defaultChatAgentUrl = 'https://cms.chatagent.io';
 
 export const Config = {
-  noco: {
-    config: {
-      url: process.env.NEXT_PUBLIC_API_NOCO_URL ?? defaultNocoUrl,
-      key: process.env.NEXT_PUBLIC_API_NOCO_KEY ?? '',
-    },
-    resources: {
-      careers: '/tables/m52917yyvdgr163/records',
-    },
-  },
   chatAgent: {
     config: {
       url: process.env.NEXT_PUBLIC_API_CHATAGENT_URL ?? defaultChatAgentUrl,
--- app/services/httpClient.ts
+++ app/services/httpClient.ts
@@ -1,13 +1,6 @@
 import axios from 'axios';
 import { Config } from '@/app/config/api';
 
-export const httpClient = axios.create({
-  baseURL: Config.noco.config.url,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export const clientChatAgent = axios.create({
   baseURL: Config.chatAgent.config.url,
   headers: {
